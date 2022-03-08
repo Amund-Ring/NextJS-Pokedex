fc74bdcbab4c4d5a286b9ea0694620ca44c25043
Amund-Ring <60844467+Amund-Ring@users.noreply.github.com>
2022-03-08 21:59:21 +0100

    React Spring

--- components/Layout.js
+++ components/Layout.js
@@ -11,7 +11,7 @@ export default function Layout({ title, children }) {
         <title>{capitalize(title)}</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main className='container mx-auto min-h-screen min-w-full p-4 pt-8'>
+      <main className='container mx-auto min-h-screen min-w-full p-4 pt-8 overflow-hidden'>
         <h1 className='mb-10 mt-6 text-center text-4xl text-white drop-shadow-md'>
           NextJS Pokedex
         </h1>
--- pages/pokemon.js
+++ pages/pokemon.js
@@ -1,56 +1,93 @@
+import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { AiOutlineHome } from 'react-icons/ai';
+import { useTransition, animated } from 'react-spring';
+import { useRouter } from 'next/router';
+
 import Layout from '../components/Layout';
 
 export default function pokemon({ pokemon }) {
+  const [isVisible, setIsVisible] = useState(true);
+  const router = useRouter();
+
+  const transition = useTransition(isVisible, {
+    from: { x: -1500, opacity: 0 },
+    enter: { x: 0, opacity: 1 },
+    leave: { x: 1500, opacity: 0 }
+  });
+
+  const slideAndNavigate = () => {
+    setIsVisible(v => !v);
+    setTimeout(() => {
+      router.push('/');
+    }, 300);
+  };
+
   return (
     <Layout title={pokemon.name}>
-      <div className='top-14 w-32 text-center ml-8 invisible md:visible absolute'>
-        <Link href='/'>
-          <a className='flex items-center gap-2 text-2xl justify-center text-white'>
-            <AiOutlineHome />
-            Home
-          </a>
-        </Link>
+      <div className='invisible absolute top-14 ml-8 w-32 text-center md:visible'>
+        {/* <Link href='/'> */}
+        <a
+          onClick={slideAndNavigate}
+          className='flex cursor-pointer items-center justify-center gap-2 text-2xl text-white'
+        >
+          <AiOutlineHome />
+          Home
+        </a>
+        {/* </Link> */}
       </div>
-      <div className='mx-auto max-w-md rounded-[22px] border-[26px] border-amber-300 bg-amber-300 shadow-xl'>
-        <div className='h-full w-full rounded-[22px] bg-orange-500 p-6'>
-          <h1 className='mb-2 text-center text-4xl capitalize text-white drop-shadow-md'>
-            {pokemon.name}
-          </h1>
 
-          <div className='relative mx-auto aspect-square w-full'>
-            <Image src={pokemon.image} alt={pokemon.name} layout='fill' />
-          </div>
+      {transition((style, item) =>
+        item ? (
+          <animated.div
+            style={style}
+            className='item mx-auto max-w-md rounded-[22px] border-[26px] border-amber-300 bg-amber-300 shadow-xl'
+          >
+            <div className='h-full w-full rounded-[22px] bg-orange-500 p-6'>
+              <h1 className='mb-2 text-center text-4xl capitalize text-white drop-shadow-md'>
+                {pokemon.name}
+              </h1>
 
-          <div className='flex justify-center gap-14 pt-3'>
-            <p>
-              <span className='mr-2 font-bold'>Weight:</span>
-              {pokemon.weight / 10} kg
-            </p>
-            <p>
-              <span className='mr-2 font-bold'>Height:</span>
-              {pokemon.height * 10} cm
-            </p>
-          </div>
+              <div className='relative mx-auto aspect-square w-full'>
+                <Image src={pokemon.image} alt={pokemon.name} layout='fill' />
+              </div>
 
-          <div className='mb-2 flex justify-center gap-24 pt-6'>
-            <div>
-              <h2 className='mb-1 text-xl text-white'>Types:</h2>
-              {pokemon.types.map(({ type }, index) => (
-                <p key={index}>{type.name}</p>
-              ))}
+              <div className='flex justify-center gap-14 pt-3'>
+                <p>
+                  <span className='mr-2 font-bold'>Weight:</span>
+                  {pokemon.weight / 10} kg
+                </p>
+                <p>
+                  <span className='mr-2 font-bold'>Height:</span>
+                  {pokemon.height * 10} cm
+                </p>
+              </div>
+
+              <div className='mb-2 flex justify-center gap-24 pt-6'>
+                <div>
+                  <h2 className='mb-1 text-xl text-white'>Types:</h2>
+                  {pokemon.types.map(({ type }, index) => (
+                    <p key={index} className='capitalize'>
+                      {type.name}
+                    </p>
+                  ))}
+                </div>
+                <div>
+                  <h2 className='mb-1 text-xl text-white'>Abilities:</h2>
+                  {pokemon.abilities.map(({ ability }, index) => (
+                    <p key={index} className='capitalize'>
+                      {ability.name}
+                    </p>
+                  ))}
+                </div>
+              </div>
             </div>
-            <div>
-              <h2 className='mb-1 text-xl text-white'>Abilities:</h2>
-              {pokemon.abilities.map(({ ability }, index) => (
-                <p key={index}>{ability.name}</p>
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
+          </animated.div>
+        ) : (
+          ''
+        )
+      )}
     </Layout>
   );
 }
